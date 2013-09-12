833218e417224c60afe1a61142149cbcedfc45e9
Stewart Duffey <duffey.sj@gmail.com>
2013-09-12 08:26:21 +0100

    Fix formatting in wiki controllers.js

--- app/wiki/controllers.js
+++ app/wiki/controllers.js
@@ -1,83 +1,88 @@
 'use strict'
 
-var wikiModule = angular.module('WikiModule', []);
+var wikiModule = (function(angular) {
+  var module = angular.module('WikiModule', []);
 
-wikiModule.controller('wiki.index', function($scope) {
+  module.controller('wiki.index', function($scope) {
     //
-});
+  });
 
-// if we get a 404, create new wikidoc with name from routeparams
+  // if we get a 404, create new wikidoc with name from routeparams
 
-wikiModule.controller('wiki.view', function($scope, $routeParams, pouchWrapper) {
+  module.controller('wiki.view', function($scope, $routeParams, pouchWrapper) {
     pouchWrapper.get($routeParams.name).then(function(res) {
-        $scope.wikidoc = res;
+      $scope.wikidoc = res;
     }, function(reason) {
-        console.log(reason);
-        $scope.wikidoc = {
-            title: $routeParams.name,
-            body: ''
-        };
+      console.log(reason);
+      $scope.wikidoc = {
+        title: $routeParams.name,
+        body: ''
+      };
     });
 
     $scope.editUrl = 
-        '/#/project/' + encodeURIComponent($routeParams.project) + 
-        '/wiki/' + encodeURIComponent($routeParams.name) + '/edit';
-});
+      '/#/project/' + encodeURIComponent($routeParams.project) + 
+      '/wiki/' + encodeURIComponent($routeParams.name) + '/edit';
+  });
 
-wikiModule.controller('wiki.edit', function($scope, $routeParams, $location, pouchWrapper) {
+  module.controller('wiki.edit', function($scope, $routeParams, $location, pouchWrapper) {
 
     var viewUrl = '/project/' + $routeParams.project + '/wiki/' + $routeParams.name;
     $scope.viewUrl = 
-        '/#/project/' + encodeURIComponent($routeParams.project) + 
-        '/wiki/' + encodeURIComponent($routeParams.name);
+      '/#/project/' + encodeURIComponent($routeParams.project) + 
+      '/wiki/' + encodeURIComponent($routeParams.name);
 
     pouchWrapper.get($routeParams.name).then(function(res) {
-        $scope.wikidoc = res;
+      $scope.wikidoc = res;
     }, function(reason) {
-        console.log(reason);
-        $scope.wikidoc = {
-            title: $routeParams.name,
-            body: ''
-        };
+      console.log(reason);
+      $scope.wikidoc = {
+        title: $routeParams.name,
+        body: ''
+      };
     });
 
     /*$scope.wikidoc = {
-        title: decodeURIComponent($routeParams.name),
-        content: 'As a person I want to succeed so that I can win...'
+      title: decodeURIComponent($routeParams.name),
+      content: 'As a person I want to succeed so that I can win...'
     };*/
 
     /*$scope.$watch('wikidoc.content', function() {
-        console.log($scope.wikidoc.content);
+      console.log($scope.wikidoc.content);
     });*/
 
     $scope.editComplete = function() {
-        var doc = $scope.wikidoc;
-        doc._id = encodeURIComponent(doc.title);
+      var doc = $scope.wikidoc;
+      doc._id = encodeURIComponent(doc.title);
 
-        pouchWrapper.put(doc).then(function(res) {
-            console.log("put: ");
-            console.log(res);
-            $location.path(viewUrl);
-        }, function(reason) {
-            console.log(reason);
-        });
+      pouchWrapper.put(doc).then(function(res) {
+        console.log("put: ");
+        console.log(res);
+        $location.path(viewUrl);
+      }, function(reason) {
+        console.log(reason);
+      });
     };
-});
+  });
 
-wikiModule.config(
+  module.config(
     function($routeProvider) {
-        $routeProvider
-            .when('/project/:project/wiki', {
-                controller: 'wiki.index',
-                templateUrl: 'wiki/views/index.html'
-            })
-            .when('/project/:project/wiki/:name', {
-                controller: 'wiki.view',
-                templateUrl: 'wiki/views/view.html'
-            })
-            .when('/project/:project/wiki/:name/edit', {
-                controller: 'wiki.edit',
-                templateUrl: 'wiki/views/edit.html'
-            });
+      $routeProvider
+        .when('/project/:project/wiki', {
+          controller: 'wiki.index',
+          templateUrl: 'wiki/views/index.html'
+        })
+        .when('/project/:project/wiki/:name', {
+          controller: 'wiki.view',
+          templateUrl: 'wiki/views/view.html'
+        })
+        .when('/project/:project/wiki/:name/edit', {
+          controller: 'wiki.edit',
+          templateUrl: 'wiki/views/edit.html'
+        });
     }
-);
+  ); 
+
+  return module; 
+})(angular);
+
